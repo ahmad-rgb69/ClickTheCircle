617cfc5d78b1cfd5faf0149561e82ad56e587cd2
ahmad-rgb69 <aldywalker123@gmail.com>
2026-05-07 07:01:39 +0800

    update tampilan room.view.php

--- views/lobby.view.php
+++ views/lobby.view.php
@@ -216,10 +216,10 @@ $presetItems   = avatar_presets_list();
         <div id="about-section" class="w-full max-w-4xl mt-2 hidden transition-all duration-300 ease-in-out transform scale-95 opacity-0 bg-gray-300 border-2 border-gray-600 rounded-md shadow-xl p-6 text-gray-800">
             <h3 class="text-2xl font-bold border-b pb-2 mb-4 border-gray-500 text-gray-700">About</h3>
             <p class="text-xs text-gray-600 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                ClickTheCircle adalah game adu ketangkasan berbasis web yang dirancang untuk menguji kecepatan reaksi dan akurasi.
+                Di game ini, pemain bisa duel secara real-time bareng pemain lain dalam satu ruangan (private room). Intinya simpel: Siapa yang paling cepat dapat target dan poin paling tinggi, dia yang menang.
+                Dikembangkan dengan gaya visual Neubrutalism, game ini fokus pada gameplay yang kompetitif, kustomisasi penuh, dan performa yang responsif.
+
             </p>
         </div>
         
